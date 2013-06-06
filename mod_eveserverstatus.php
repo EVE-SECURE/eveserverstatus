e68807e10a50dc2667112d70a336a53971a6b1c9
utkin.peter@gmail.com <utkin.peter@gmail.com@b83b373f-646f-89e8-12e9-d7ab62b5089d>
2013-06-06 16:42:18 +0000

    цвета

--- mod_eveserverstatus.php
+++ mod_eveserverstatus.php
@@ -13,9 +13,6 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-#$timeLimit=10;
-#header('Refresh: ' . $timeLimit);
-
 $url='https://api.eveonline.com/server/ServerStatus.xml.aspx';
 
 $xml = simplexml_load_file($url);
@@ -29,9 +26,6 @@ $currentTime=preg_replace('/(\d\d:\d\d):\d\d/',"\\1",$currentTime);
 $serverOpen=preg_replace('/True/',"<span style='color:#00FF00;'><b>Online</b></span>",$serverOpen);
 $serverOpen=preg_replace('/False/',"<span style='color:#FF0000;'><b>Offline</b></span>", $serverOpen);
 
-
 echo JText::_("<ul class='eve-status'><li>Статус сервера:&nbsp;$serverOpen</li>");
 echo JText::_("<li>Дата и время сервера:&nbsp;<span class='blue-text'><b>$currentTime</b></span></li>");
 echo JText::_("<li>Игроков онлайн:&nbsp;<span class='blue-text'><b>$onlinePlayers</b></span></li></ul>");
-
-
