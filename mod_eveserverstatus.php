635f6d9fe249018f4586b274eb38d3c346178512
utkin.peter@gmail.com <utkin.peter@gmail.com@b83b373f-646f-89e8-12e9-d7ab62b5089d>
2013-06-06 15:51:06 +0000

    добавил стиль, цвет статуса сервера и выдачу результата списком ul/li

--- mod_eveserverstatus.php
+++ mod_eveserverstatus.php
@@ -13,6 +13,9 @@
 
 defined('_JEXEC') or die('Restricted access');
 
+#$timeLimit=10;
+#header('Refresh: ' . $timeLimit);
+
 $url='https://api.eveonline.com/server/ServerStatus.xml.aspx';
 
 $xml = simplexml_load_file($url);
@@ -21,15 +24,23 @@ $currentTime=$xml->currentTime;
 $serverOpen=$xml->result->serverOpen;
 $onlinePlayers=$xml->result->onlinePlayers;
 
+$currentTime=preg_replace('/(\d\d:\d\d):\d\d/',"\\1",$currentTime);
 
-#2013-05-22 03:38:26
-#
-$currentTime=preg_replace('/(\d\d:\d\d):\d\d/','\\1',$currentTime);
-
-$serverOpen=preg_replace('/True/','online',$serverOpen);
-$serverOpen=preg_replace('/False/', 'offline', $serverOpen);
+$serverOpen=preg_replace('/True/',"<span style='color:#00FF00;'><b>Online</b></span>",$serverOpen);
+$serverOpen=preg_replace('/False/',"<span style='color:#FF0000;'><b>Offline</b></span>", $serverOpen);
 
 
-echo JText::_("eveTime: $currentTime \n<br/>");
-echo JText::_("Статус сервера:&nbsp;$serverOpen \n<br/>");
-echo JText::_("Игроков онлайн:&nbsp;$onlinePlayers \n<br/>");
+echo JText::_("<ul class='eve-status'><li>Статус сервера:&nbsp;$serverOpen</li>");
+echo JText::_("<li>Игроков онлайн:&nbsp;<b>$onlinePlayers</b></li>");
+echo JText::_("<li>Игровое время:&nbsp;<b>$currentTime</b></li></ul>");
+
+#$l="<ul class='eve-status'><li>Статус сервера:&nbsp;";
+#if ($vals[4]['value']=="True")
+#{
+# $l.="<span style='color:#00FF00;'><b>Online</b></span></li>";
+#}
+#else
+#{
+#    $l.="<span style='color:#FF0000;'><b>Offline</b></span></li>";
+#}
+#$l.="<li>Игроков онлайн:&nbsp;<b>".$vals[6]['value']."</b></li></ul>";
