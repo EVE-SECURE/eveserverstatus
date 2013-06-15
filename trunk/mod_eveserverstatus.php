60ae2557b9694246ffbfe35d3ee6301b74012d6d
utkin.peter@gmail.com <utkin.peter@gmail.com@b83b373f-646f-89e8-12e9-d7ab62b5089d>
2013-06-15 17:09:04 +0000

    ajax v0.0.2 use plugin ==jQuery Easy== http://www.simplifyyourweb.com/index.php/downloads/category/8-loading-jquery

--- trunk/mod_eveserverstatus.php
+++ trunk/mod_eveserverstatus.php
@@ -1,26 +1,45 @@
 <?php
 
 defined('_JEXEC') or die('Restricted access');
+#<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+#<script type="text/javascript">
+#jQuery.noConflict();
+#</script>
+#
+#<script type="text/javascript" src="media/jui/js/jquery.min.js"></script>
+
+#<script type="text/javascript" src="media/jui/js/jquery.min.js"></script>
+#<script type="text/javascript" src="media/jui/js/jquery-noconflict.js"></script>
+
+#<script type="text/javascript" src="media/jui/js/jquery.min.js"></script>
+
 ?>
+
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 
 <div id="contentq"></div>
 
    <script>
+
+(function($){
+    //plugin code
+
+
     function show()
     {
-    $.ajax({
+    jQuery.ajax({
     url: "modules/mod_eveserverstatus/echostat.php",
     cache: false,
     success: function(html){
-    $("#contentq").html(html);
+    jQuery("#contentq").html(html);
 }
     });
 }
-$(document).ready(function(){
+jQuery(document).ready(function(){
     show();
     setInterval('show()',60000);
 });
+})(jQuery)
     </script>
 
 
